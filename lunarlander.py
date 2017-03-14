f63c4c96d1860095fb08f6b481cb3e231c970114
Minh Le <lenguyetminh1998@gmail.com>
2017-03-14 14:46:12 -0400

    Blit in image and rocket movement

--- lunarlander.py
+++ lunarlander.py
@@ -1,3 +1,5 @@
+#rocket can kinda move across screen 
+
 import pygame
 import time
 import random
@@ -6,11 +8,17 @@ pygame.init()
 #define variables to be used in the program
 display_width = 400
 display_height = 600
-myfont = pygame.font.SysFont(None, 25)
 myclock = pygame.time.Clock()
 white = (255,255,255)
 green = (0,155,0)
 black = (0,0,0)
+introimg = pygame.image.load("C:\\Users\\lengu\\Desktop\\Pygame\\700862.jpg")
+introimg2 = pygame.transform.scale(introimg, (1100,600))
+rocket = pygame.image.load("C:\\Users\\lengu\\Desktop\\Pygame\\rocket.png")
+rocket2 = pygame.transform.scale(rocket, (100, 100))
+asteroid = pygame.image.load("C:\\Users\\lengu\\Desktop\\Pygame\\asteroid.png")
+asteroid2 = pygame.transform.scale(asteroid, (30,30))
+asteroid3 = pygame.transform.rotate(asteroid2, -90)
 
 #create the screen
 gameDisplay = pygame.display.set_mode((display_width, display_height))
@@ -19,54 +27,97 @@ gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Lunar')
 
 #Function 1: Allow message to be displayed
-def message_to_screen(message, color):
+def message_to_screen(message, color, widthdis, heightdis, fontsize):
+	myfont = pygame.font.SysFont(None, fontsize)
 	screen_text = myfont.render(message, True, color)
-	gameDisplay.blit(screen_text, [display_width/2, display_height/2])
+	gameDisplay.blit(screen_text, [widthdis, heightdis])
 
-#intro function
+#Function 2: Intro Screen
 def gameIntro():
-    intro = True;
-    while (intro):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    pygame.quit()
-                    quit()
-                if event.key == pygame.K_p:
-                    intro = False
+	intro = True;
+	while (intro):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					pygame.quit()
+					quit()
+				if event.key == pygame.K_p:
+					intro = False
 
-        gameDisplay.fill(white)
-        message_to_screen("LUNAR", green)
-        message_to_screen("Press P to Play or Q to Quit", black)
-        pygame.display.update()
-        myclock.tick(15)
+		gameDisplay.blit(introimg2, (-50,0))
+		gameDisplay.blit(rocket2, (150, 500))
+		message_to_screen("LUNAR LANDER", white, 90, 20, 40)
+		message_to_screen("Developed by M. Le", white, 120, 60, 20)
+		message_to_screen("Instructions:", white, 20, 240, 20)
+		message_to_screen("1. You have 150 protection", white, 30, 260, 20)
+		message_to_screen("2. Rock hit = -10 points", white, 30, 275, 20)
+		message_to_screen("3. Press <- to move left, -> to move right", white, 30, 290, 20)
+		message_to_screen("4. Press P to play, Q to quit", white, 30, 305, 20)
+		pygame.display.update()
+		myclock.tick(15)
+
+
 
 #Function 3: Game Loop
 def gameLoop():
-    gameExit = False                        #false condition prevent display before game loop
-    gameOver = False                        #false condition prevent display before game loop
+	gameExit = False
+	gameOver = False
+	xroc = 150
+	yroc = 500
+	xroc_change = 0
+	yroc_cahnge = 0
 
-    #while user plays game
-    while not gameExit:
-        for event in pygame.event.get:
-            if event.type == game.Quit:     #if user decide to end game
-                gameOver = False            #gameover screen is false because the user doesn't lose the game
-                gameExit = True             #gameExit screen is true since game is quit
+	#while user plays game
+	while not gameExit:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					pygame.quit()
+					quit()
+				if event.key == pygame.K_RIGHT:
+					xroc_change = 5
+				if event.key == pygame.K_LEFT:
+					xroc_change = -5
 
 
+		#allow moving until user releases from keyboard
+		xroc += xroc_change
+
+		#prevent rocket to disappear from screen
+		if xroc > 325: xroc = 325
+		if xroc < -20: xroc = -20
+
+		#blitting out images
+		gameDisplay.blit(introimg2, (0,0))
+		gameDisplay.blit(rocket2, (xroc, yroc))
+		gameDisplay.blit(asteroid3, (0, 0))
+		position = asteroid3.get_rect()
+		gameDisplay.blit(asteroid3, position)
+		pygame.display.update()
 
 
-        #specify 15 frame per second
-        myclock.tick(30)
+		for x in range(100):
+			gameDisplay.blit(introimg2, position, position)
+			position = position.move(5, 5)
+			gameDisplay.blit(asteroid3, position)
+			pygame.display.update()
+			#pygame.time.delay(100)
 
-    #while user quits game
-    pygame.quit()
-    quit()
+		myclock.tick(30)
+
+	#while user quits game
+	pygame.quit()
+	quit()
 
 
+#Part 4: Calling all the created function
+gameIntro()
+gameLoop()
 
-gameIntro()                                 #call the intro screen
-gameLoop()                                  #initialize the game loop
+
